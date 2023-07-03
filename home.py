fee21056fecdc0792924f8c85cfcb7317bed73de
luqmanhmj <89148074+luqmanhmj@users.noreply.github.com>
2023-07-03 14:24:33 +0800

    Update home.py

--- home.py
+++ home.py
@@ -173,50 +173,10 @@ def showEnrichPage() :
     st.subheader(':spiral_calendar_pad: Select Campaign and Date')
 
     # Create equal size columns 
-    column_1, column_2, column_3 = st.columns([1, 1, 1])
-
-    # In the first column
-    with column_1 :
-
-        # When there is a proper campaign list 
-        if campaign_check :
-
-            # Use the list of campaign name
-            #campaign_option = list(campaign_data['Campaign Name'])
-            campaign_option = ['Lead Gen - Lead Pool 2023', 
-                               'Website Conversion: Rally Starter Edition (completed)', 
-                               'Website Conversion - Lead Pool 2023',
-                               'Lead Gen: Rally Starter Edition June 2023',
-                               'Website Conversion: Rally Starter Edition June 2023']
-            
-        
-        # When no proper campaign list found
-        else :
-
-            # No options should be available
-            campaign_option = []
-            
-
-        # Create select box for campaign options
-        campaign_name = st.selectbox(
-            label = 'Campaign :',
-            options = campaign_option,
-            disabled = not campaign_check
-        )
-
-        # Initialize campaign id holder
-        campaign_info = ''
-        campaign_group = ''
-
-        # When there is a campaign selected
-        if campaign_name :
-
-            # Get the selected campaign's info
-            campaign_info = list(campaign_data[campaign_data['Campaign Name'] == campaign_name]['Campaign ID'])[0]
-            campaign_group = list(campaign_data[campaign_data['Campaign Name'] == campaign_name]['Campaign Group'])[0]
+    column_1, column_2 = st.columns([1, 1])
 
     # In the second column
-    with column_2 :
+    with column_1 :
 
         # When there is a proper campaign list 
         if campaign_check :
@@ -281,6 +241,8 @@ def showEnrichPage() :
         )
 
         # Initialize creative id holder
+        campaign_info = ''
+        campaign_group = ''
         creative_id = ''
         solution_area = ''
 
@@ -288,11 +250,13 @@ def showEnrichPage() :
         if creative_name :
 
             # Get the selected creative's info
+            campaign_info = list(campaign_data[campaign_data['Campaign Name'] == campaign_name]['Campaign ID'])[0]
+            campaign_group = list(campaign_data[campaign_data['Campaign Name'] == campaign_name]['Campaign Group'])[0]
             creative_id = list(campaign_data[campaign_data['Creative Name'] == creative_name]['Creative ID'])[0]
             solution_area = list(campaign_data[campaign_data['Creative Name'] == creative_name]['Solution Area'])[0]
 
     # In the third column
-    with column_3 :
+    with column_2 :
 
         # Create date picker for extract date
         extract_date = st.date_input(
